fe8335f6a8c94618d7b9d20cdf5a1104a7be676b
Allison Collier <allisc9@vt.edu>
2020-09-01 06:04:47 -0400

    add: build directory and updated gitignore

---
+++ build/precache-manifest.8bdbb4928ea724e37beab2b8cd6bc066.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77ef904db7d7a01bd1aeaa53876bcd3",
+    "url": "/apodgallery/index.html"
+  },
+  {
+    "revision": "a1243930d1f51def3141",
+    "url": "/apodgallery/static/css/main.e786678b.chunk.css"
+  },
+  {
+    "revision": "596c31f2ff5f223dd1a9",
+    "url": "/apodgallery/static/js/2.330c6949.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apodgallery/static/js/2.330c6949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1243930d1f51def3141",
+    "url": "/apodgallery/static/js/main.0ee992e9.chunk.js"
+  },
+  {
+    "revision": "332308718c9cd85760a9",
+    "url": "/apodgallery/static/js/runtime-main.72783bd8.js"
+  }
+]);
